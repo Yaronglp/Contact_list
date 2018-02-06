adb05534e28c99d7265ae7b68c825ac25ed0bd81
yaron g <yaronglp@gmail.com>
2018-02-06 15:39:03 +0200

    Fix bug - loading of an invalid picture

--- src/js/components/member/MemberIcon.js
+++ src/js/components/member/MemberIcon.js
@@ -1,9 +1,16 @@
 import Icon from '../general/Icon';
 
 const MemberIcon = (props) => {
-    const {picture, fullname, classProp, isMemberList, showFullNameLabel, clickHandler, itemId} = props;
+    const {fullname, picture, classProp, isMemberList, showFullNameLabel, clickHandler, itemId} = props;
 
-    return <div className={classProp} onClick={() => clickHandler(itemId)}>
+    let onImgError = (e) => {
+        if(e.target.nodeName === 'IMG'){
+            console.error('You are trying to use an invalid picture. please change it to a valid one');
+            e.target.src = MemberIcon.defaultProps.picture;
+        }
+    };
+
+    return <div className={classProp} onClick={() => clickHandler(itemId)} onError={onImgError}>
                 <Icon classname={isMemberList ? "img_icon list_member_pic" : "img_icon"}
                       url={picture}
                       title={fullname}/>
