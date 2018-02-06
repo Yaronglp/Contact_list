4b297a2869b5c513b08bd3c2544941360ce1c59f
yaron g <yaronglp@gmail.com>
2018-02-06 17:27:36 +0200

    Fix for code duplication

--- src/js/components/member/AddRemoveMember.js
+++ src/js/components/member/AddRemoveMember.js
@@ -1,14 +1,19 @@
 import Icon from '../general/Icon';
 
-const AddRemoveMember = ({handleRmvClick, handleAddClick, permitToRemove}) =>
-    <div className="additional_btn">
-        <div className={permitToRemove ? "add_remove_btn_container member_btn remove_member_btn" :
-            "add_remove_btn_container member_btn remove_member_btn disabled"} onClick={handleRmvClick}>
-            <Icon classname="add_remove_btn remove_btn truncate"/>
+const AddRemoveMember = ({handleRmvClick, handleAddClick, permitToRemove}) => {
+
+    let remClsName = `add_remove_btn_container member_btn remove_btn_container " ${!permitToRemove ? "disabled" : null}`;
+
+    return (
+        <div className="additional_btn">
+            <div className={remClsName} onClick={handleRmvClick}>
+                <Icon classname="add_remove_btn remove_btn truncate"/>
+            </div>
+            <div className="add_remove_btn_container member_btn add_btn_container" onClick={handleAddClick}>
+                <Icon classname="add_remove_btn add_btn truncate"/>
+            </div>
         </div>
-        <div className="add_remove_btn_container member_btn add_member_btn" onClick={handleAddClick}>
-            <Icon classname="add_remove_btn add_btn truncate"/>
-        </div>
-    </div>;
+    );
+};
 
 export default AddRemoveMember;
